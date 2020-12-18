63e0c4126b288fcd3e477dc07dbdc04922e09830
João Vítor Marquim <joaomarquim@ioasys.com.br>
2020-12-18 20:41:56 -0300

    feat(email): added ses to send emails

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,6 +1,31 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'us-east-2' });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: 'jvmarquim@gmail.com',
+    Destination: {
+      ToAddresses: ['jvmarquim@gmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from me',
+        },
+      },
+      Subject: {
+        Data: 'Test Mail',
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
